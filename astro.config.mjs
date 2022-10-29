2b971ec551d9f71f2a4208e6f272f1b35939e2f4
Roel Bruggink <roel@jaroel.nl>
2022-10-29 23:11:50 +0200

    Replace SolidJS with nanostores

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 import { defineConfig } from "astro/config";
-import solid from "@astrojs/solid-js";
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-    solid(),
     tailwind(),
     image({
       serviceEntryPoint: "@astrojs/image/sharp",
--- src/stores.ts
+++ src/stores.ts
@@ -1,14 +1,3 @@
-import { createEffect } from "solid-js";
-import { createStore, SetStoreFunction, Store } from "solid-js/store";
+import { atom } from "nanostores";
 
-
-export function createSessionStore<T extends object>(
-  initState: T
-): [Store<T>, SetStoreFunction<T>] {
-  const [state, setState] = createStore(initState);
-  if (sessionStorage.appstate) setState(JSON.parse(sessionStorage.appstate));
-  createEffect(() => (sessionStorage.appstate = JSON.stringify(state)));
-  return [state, setState];
-}
-
-export const [appState, setAppState] = createSessionStore({ video: false });
+export const video = atom(false);
