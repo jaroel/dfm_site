aa0750044111a917551f7a867aafd5cafb1099a7
Roel Bruggink <roel@jaroel.nl>
2023-01-13 17:23:31 +0100

    Use solid-js to render UZG listing. first draft

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,19 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import image from "@astrojs/image";
+/* eslint-disable n/file-extension-in-import */
+import {defineConfig} from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import image from '@astrojs/image'
+
+// https://astro.build/config
+import solidJs from '@astrojs/solid-js'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     tailwind(),
     image({
-      serviceEntryPoint: "@astrojs/image/sharp",
+      serviceEntryPoint: '@astrojs/image/sharp',
     }),
+    solidJs(),
   ],
-});
+  // Vite: {ssr: {external: ['ftp-ts']}},
+})
--- src/stores.ts
+++ src/stores.ts
@@ -1,3 +1,18 @@
-import { atom } from "nanostores";
+import {atom, action} from 'nanostores'
+import {useStore} from '@nanostores/solid'
 
-export const video = atom(false);
+// Homepage
+export const video = atom(false)
+
+// Uitzending gemist
+const _playerUrl = atom<string | undefined>()
+export const setPlayerUrl = action(_playerUrl, 'setPlaying', (_store, value: string | undefined) => {
+  _playerUrl.set(value)
+})
+export const playerUrl = useStore(_playerUrl)
+
+const _audioStreamState = atom<'stopped' | 'loading' | 'playing'>('stopped')
+export const setAudioStreamState = action(_audioStreamState, 'setAudioStreamState', (_store, value: 'stopped' | 'loading' | 'playing') => {
+  _audioStreamState.set(value)
+})
+export const audioStreamState = useStore(_audioStreamState)
