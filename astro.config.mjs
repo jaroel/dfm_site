ceaed6ae82873ed703a22883988c606d985f006f
Roel Bruggink <roel@jaroel.nl>
2023-05-05 16:00:23 +0200

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
-import {defineConfig} from 'astro/config'
-import {sharpImageService} from 'astro/assets'
+import {defineConfig, sharpImageService} from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import solidJs from '@astrojs/solid-js'
 import node from '@astrojs/node'
