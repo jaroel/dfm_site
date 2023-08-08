9f9629a5214430e9c226e655640c22d39cd0f3ed
Roel Bruggink <roel@jaroel.nl>
2023-08-09 00:53:18 +0200

    Make things work for https://dfmsite.jaroel.nl/

--- README.md
+++ README.md
@@ -93,8 +93,84 @@ LEPTOS_RELOAD_PORT="3001"
 
 Finally, run the server binary.
 
-You can also run `cargo leptos serve --release` instead.
+
+## Development
 
 During development, in two terminals:
+
+```bash
 ./tailwindcss -i ./style/dfm.in.css -o ./style/dfm.out.css --watch
 cargo leptos watch
+```
+
+
+## Generate binaries for Linux:
+i686 on Digital Ocean
+
+Setup:
+
+```bash
+brew tap messense/macos-cross-toolchains
+#brew install x86_64-unknown-linux-gnu
+#rustup target add x86_64-unknown-linux-gnu
+
+brew install i686-unknown-linux-gnu
+rustup target add i686-unknown-linux-gnu
+```
+
+Build:
+```bash
+#RUSTC_WRAPPER= LEPTOS_BIN_TARGET_TRIPLE=x86_64-unknown-linux-gnu CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_LINKER=x86_64-linux-gnu-gcc cargo leptos build --release
+
+#❯ file target/server/x86_64-unknown-linux-gnu/release/dfm_site
+#target/server/x86_64-unknown-linux-gnu/release/dfm_site: ELF 64-bit LSB pie executable, x86-64, version 1 (SYSV), dynamically linked, interpreter /lib64/ld-linux-x86-64.so.2, for GNU/Linux 2.6.16, with debug_info, not stripped
+
+
+RUSTC_WRAPPER= LEPTOS_BIN_TARGET_TRIPLE=i686-unknown-linux-gnu CARGO_TARGET_I686_UNKNOWN_LINUX_GNU_LINKER=i686-unknown-linux-gnu-gcc cargo leptos build --release
+```
+
+Artifects:
+```bash
+❯ file target/server/i686-unknown-linux-gnu/release/dfm_site
+target/server/i686-unknown-linux-gnu/release/dfm_site: ELF 32-bit LSB pie executable, Intel 80386, version 1 (SYSV), dynamically linked, interpreter /lib/ld-linux.so.2, for GNU/Linux 3.2.101, with debug_info, not stripped
+
+❯ ls -l target/server/i686-unknown-linux-gnu/release/dfm_site
+-rwxr-xr-x@ 1 roel  staff  20297768 Aug  9 00:39 target/server/i686-unknown-linux-gnu/release/dfm_site*
+
+❯ ls -l target/site/pkg/
+total 1256
+-rw-r--r--@ 1 roel  staff    7437 Aug  9 00:39 dfm_site.css
+-rw-r--r--@ 1 roel  staff   45396 Aug  9 00:39 dfm_site.js
+-rw-r--r--@ 1 roel  staff  583728 Aug  9 00:39 dfm_site.wasm
+```
+
+Deploy:
+```bash
+scp -C -r target/site jaroel.nl:/home/leptos/target/
+scp -C target/server/i686-unknown-linux-gnu/release/dfm_site jaroel.nl:/home/leptos/
+```
+
+
+## FTP Mount
+The UZG listing data...
+Setup:
+```bash
+sudo apt-get install curlftpfs
+```
+Add to ~leptos/.netrc
+```text
+machine ftp.example.com login your-user-name password your-password
+```
+
+Mount:
+```bash
+su leptos
+curlftpfs -o uid=$(id -u leptos) ftp://ftp.example.com /home/leptos/uzg_data
+
+```
+
+Unmount:
+```bash
+fusermount -u /home/leptos/uzg_data
+
+```
--- src/app.rs
+++ src/app.rs
@@ -295,7 +295,7 @@ fn HomePage(cx: Scope) -> impl IntoView {
     <div class="mt-10 flex justify-center">
       <div style="width: 1085; height: 656">
         <Image
-          src="/programmering/WeekprogrammaDFM-20230304.jpg"
+          src="/programmering/weekprogrammaDFM-20230304.jpg"
           alt="Programmering vanaf 4 maart 2023 van Dinxperlo FM "
           width=1085
           height=656
@@ -314,21 +314,21 @@ fn HomePage(cx: Scope) -> impl IntoView {
       <h2 class="text-2xl">"Dinxper FM wordt mede mogelijk gemaakt door"</h2>
       <div class="flex flex-row flex-wrap justify-evenly">
         <Sponsor title="De markt van Dinxperlo".into() href="https://www.facebook.com/markt.dinxperlo/".into()>
-          <Image src="/sponsors/makt.jpg" alt="De markt van Dinxperlo" width=250 height=250 quality=100/>
+          <Image src="/sponsors/makt.jpg" alt="De markt van Dinxperlo" width=250 height=250 quality=100 blur=true />
         </Sponsor>
         <Sponsor title="Naaiatelier Monique Harmsen".into() href="http://www.naaiateliermoniqueharmsen.nl/".into()>
-          <Image src="/sponsors/logo13.jpg" alt="Naaiatelier Monique Harmsen" width=250 height=250 quality=100/>
+          <Image src="/sponsors/logo13.jpg" alt="Naaiatelier Monique Harmsen" width=250 height=250 quality=100 blur=true />
 
         </Sponsor>
         <Sponsor title="Podesta event supplies".into() href="https://podesta.nl/".into()>
-          <Image src="/sponsors/logo15.jpg" alt="Podesta event supplies" width=250 height=250 quality=100/>
+          <Image src="/sponsors/logo15.jpg" alt="Podesta event supplies" width=250 height=250 quality=100 blur=true />
 
         </Sponsor>
         <Sponsor
           title="Adviesbureau Roenhorst Dinxperlo".into()
           href="https://www.facebook.com/Adviesbureau-Roenhorst-Dinxperlo-234893600009274/".into()
         >
-          <Image src="/sponsors/logo16.jpg" alt="Adviesbureau Roenhorst Dinxperlo" width=250 height=250 quality=100/>
+          <Image src="/sponsors/logo16.jpg" alt="Adviesbureau Roenhorst Dinxperlo" width=250 height=250 quality=100 blur=true />
         </Sponsor>
         <Sponsor
           title="Landwinkel Smits Groenten & Fruit | Smits Groentekwekerij B.V. | Smits Groenten en Fruit".into()
@@ -355,7 +355,7 @@ fn HomePage(cx: Scope) -> impl IntoView {
           />
         </Sponsor>
         <Sponsor title="Tiggelovend-Kok B.V.".into() href="https://www.tiggelovend-kok.nl/".into()>
-          <Image src="/sponsors/logo22.jpg" alt="Tiggelovend-Kok B.V." width=250 height=250 quality=100/>
+          <Image src="/sponsors/logo22.jpg" alt="Tiggelovend-Kok B.V." width=250 height=250 quality=100 blur=true />
         </Sponsor>
         <Sponsor
           title="VVNF: Vereniging Viering Nationale Feest- en Gedenkdagen Dinxperlo".into()
@@ -373,23 +373,23 @@ fn HomePage(cx: Scope) -> impl IntoView {
           title="Jumbo Dinxperlo Leussink".into()
           href="https://www.jumbo.com/content/jumbo-dinxperlo-heelweg/".into()
         >
-          <Image src="/sponsors/jumbo.jpg" alt="Jumbo Dinxperlo Leussink" width=250 height=250 quality=100/>
+          <Image src="/sponsors/jumbo.jpg" alt="Jumbo Dinxperlo Leussink" width=250 height=250 quality=100 blur=true />
         </Sponsor>
         <Sponsor title="Old Dutch Dinxperlo".into() href="https://www.olddutchdinxperlo.nl/".into()>
-          <Image src="/sponsors/oldDutch.jpg" alt="Old Dutch Dinxperlo" width=250 height=250 quality=100/>
+          <Image src="/sponsors/olddutch.jpg" alt="Old Dutch Dinxperlo" width=250 height=250 quality=100 blur=true />
         </Sponsor>
         <Sponsor title="MA-Shops".into() href="https://www.ma-shops.nl/?ref=dinxperfm".into()>
-          <Image src="/sponsors/mashops.jpg" alt="MA-Shops" width=250 height=250 quality=100/>
+          <Image src="/sponsors/mashops.jpg" alt="MA-Shops" width=250 height=250 quality=100 blur=true />
         </Sponsor>
         <Sponsor
           title="Expert Dinxperlo".into()
           href="https://www.expert.nl/winkels/dinxperlo?gclid=EAIaIQobChMI1Jutxdrh4AIVzLztCh02DgFoEAAYASAAEgJwevD_BwE"
               .into()
         >
-          <Image src="/sponsors/expert.jpg" alt="Expert Dinxperlo" width=250 height=250 quality=100/>
+          <Image src="/sponsors/expert.jpg" alt="Expert Dinxperlo" width=250 height=250 quality=100 blur=true />
         </Sponsor>
         <Sponsor title="Alswin".into() href="https://www.dinxperlo.nl".into()>
-          <Image src="/sponsors/AlswinGr.jpg" alt="Alswin" width=250 height=250 quality=100/>
+          <Image src="/sponsors/alswingr.jpg" alt="Alswin" width=250 height=250 quality=100 blur=true />
         </Sponsor>
       </div>
     </div>
@@ -579,7 +579,7 @@ fn UzgListing(cx: Scope, items: Vec<Recording>) -> impl IntoView {
                                                       title=recording.title()
                                                       label=recording.label()
                                                       src=format!(
-                                                          "http://localhost:3000/uzg_data/{}", recording.file_name
+                                                          "https://dfmsite.jaroel.nl/uzg_data/{}", recording.file_name
                                                       )
 
                                                       player_src=player_src
@@ -645,7 +645,7 @@ fn UitzendingGemist(cx: Scope) -> impl IntoView {
           view! { cx, <p>"Loading (Suspense Fallback)..."</p> }
       }>
         {move || match entries.read(cx) {
-            None => view! { cx, <p>"Loading..."</p> }.into_view(cx),
+            None => view! { cx, <p>"Op dit moment zijn er geen historische uitzendingen te beluisteren."</p> }.into_view(cx),
             Some(Ok(items)) => view! { cx, <UzgListing items=items/> },
             Some(Err(_)) => todo!(),
         }}
