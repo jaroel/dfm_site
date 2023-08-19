3aa79ae1a1b3763df9837b193f3b45eefcc920fb
Roel Bruggink <roel@jaroel.nl>
2023-08-19 21:35:38 +0200

    cleanup http -> https redirect

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,14 @@
 #[cfg(feature = "ssr")]
 #[tokio::main]
 async fn main() {
-  use axum::extract::Host;
   use axum::handler::HandlerWithoutStateExt;
-  use axum::http::StatusCode;
   use axum::response::Redirect;
   use axum::routing::{get, post};
-  use axum::BoxError;
   use axum::{Router, Server};
   use axum_server;
   use axum_server::tls_rustls::RustlsConfig;
   use dfm_site::app::*;
   use dfm_site::fileserv::file_and_error_handler;
-  use http::Uri;
   use leptos::*;
   use leptos_axum::{generate_route_list, LeptosRoutes};
   use leptos_image::cache_app_images;
@@ -22,33 +18,18 @@ async fn main() {
 
   simple_logger::init_with_level(log::Level::Error).expect("couldn't initialize logging");
 
-  // Redirect 3002->3000 
-
-  fn make_https(host: String, uri: Uri) -> Result<Uri, BoxError> {
-    let mut parts = uri.into_parts();
-    parts.scheme = Some(axum::http::uri::Scheme::HTTPS);
-    if parts.path_and_query.is_none() {
-      parts.path_and_query = Some("/".parse().unwrap());
-    }
-    parts.authority = Some(format!("{host}:3000").parse()?);
-    Ok(Uri::from_parts(parts)?)
-  }
-
-  let redirect = move |Host(host): Host, uri: Uri| async move {
-    match make_https(host, uri) {
-      Ok(uri) => Ok(Redirect::permanent(&uri.to_string())),
-      Err(error) => {
-        tracing::warn!(%error, "failed to convert URI to HTTPS");
-        Err((StatusCode::BAD_REQUEST, error.to_string()))
-      }
-    }
+  let redirect = move || async move {
+    Redirect::temporary(
+      &std::env::var("PUBLIC_URL")
+        .unwrap_or("https://dfmsite.test".to_string())
+        .into_boxed_str(),
+    )
   };
 
-  let http_addr = "127.0.0.1:3002".parse::<SocketAddr>().unwrap();
+  let http_addr = std::env::var("HTTP_ADDR").unwrap_or("127.0.0.1:3002".to_string());
+  let http_addr = http_addr.parse::<SocketAddr>().expect("HTTP_ADDR");
   log!("listening on http://127.0.0.1:3002");
   let http_server = Server::bind(&http_addr).serve(redirect.into_make_service());
-
-  // HTTPS Server on port 3000 
 
   // Setting get_configuration(None) means we'll be using cargo-leptos's env values
   // For deployment these variables are:
