6edce7e961b8020fad1e5465933f2d1ffc326736
Roel Bruggink <roel@jaroel.nl>
2023-08-30 22:12:31 +0200

    Add fallback for non-wasm enabled devices
    Signed-off-by: Roel Bruggink <roel@jaroel.nl>

--- src/controls.rs
+++ src/controls.rs
@@ -20,6 +20,9 @@ pub(crate) fn Controls(
   player_state: ReadSignal<PlayerState>,
 ) -> impl IntoView {
   let (local_src, _) = create_signal(cx, src);
+  let (title, _) = create_signal(cx, title);
+  let (label, _) = create_signal(cx, label);
+
   let controls_state = move || {
     let controls_src = local_src();
     match player_state.get() {
@@ -48,77 +51,117 @@ pub(crate) fn Controls(
     }
   };
 
+  async fn has_loaded() -> bool {
+    true
+  }
+  let client_has_loaded = create_local_resource(cx, move || (), |_| has_loaded());
+
   view! { cx,
-    <button
-      title=title
-      on:click=move |_| {
-          match controls_state() {
-              ControlsState::Playing | ControlsState::Error => set_player_src(None),
-              ControlsState::Stopped | ControlsState::Loading => set_player_src(Some(local_src.get())),
-          };
-      }
+    <Suspense fallback=move || {
+        view! { cx,
+          <div class="flex items-center">
+            <span class="mr-2">{label}</span>
+            <audio
+              controls
+              controlslist="nodownload"
+              preload="none"
+              src=local_src
+              class="mr-4 rounded-full border my-1 bg-gray-100 text-gray-800 border-gray-800"
+            ></audio>
+          </div>
+        }
+    }>
+      <Show
+        when=move || { client_has_loaded.read(cx).unwrap_or(false) }
 
-      class=move || {
-          let specifics = match controls_state() {
-              ControlsState::Error => "bg-red-100 text-red-800 border-red-800",
-              ControlsState::Playing => "bg-blue-100 text-blue-800 border-blue-800",
-              ControlsState::Stopped | ControlsState::Loading => {
-                  "border-gray-800 bg-gray-800 text-white hover:bg-gray-100 hover:text-gray-800 hover:border-gray-800"
-              }
-          };
-          format!("mr-4 rounded-full border my-1 py-2 px-4 flex items-center {}", specifics)
-      }
-    >
+        fallback=move |_| {
+            view! { cx,
+              <div class="flex items-center">
+                <span class="mr-2">{label}</span>
+                <audio
+                  controls
+                  controlslist="nodownload"
+                  preload="none"
+                  src=local_src
+                  class="mr-4 rounded-full border my-1 bg-gray-100 text-gray-800 border-gray-800"
+                ></audio>
+              </div>
+            }
+        }
+      >
 
-      <span class="mr-2">{label}</span>
-      {move || match controls_state() {
-          ControlsState::Stopped => {
-              view! { cx,
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  fill="currentColor"
-                  class="bi bi-play inline"
-                  viewBox="0 0 16 16"
-                >
-                  <path d="M10.804 8 5 4.633v6.734L10.804 8zm.792-.696a.802.802 0 0 1 0 1.392l-6.363 3.692C4.713 12.69 4 12.345 4 11.692V4.308c0-.653.713-.998 1.233-.696l6.363 3.692z"></path>
-                </svg>
-              }
+        <button
+          title=title
+          on:click=move |_| {
+              match controls_state() {
+                  ControlsState::Playing | ControlsState::Error => set_player_src(None),
+                  ControlsState::Stopped | ControlsState::Loading => set_player_src(Some(local_src.get())),
+              };
           }
-          ControlsState::Error | ControlsState::Playing => {
 
-              view! { cx,
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  fill="currentColor"
-                  class="bi bi-stop-circle inline"
-                  viewBox="0 0 16 16"
-                >
-                  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"></path>
-                  <path d="M5 6.5A1.5 1.5 0 0 1 6.5 5h3A1.5 1.5 0 0 1 11 6.5v3A1.5 1.5 0 0 1 9.5 11h-3A1.5 1.5 0 0 1 5 9.5v-3z"></path>
-                </svg>
-              }
+          class=move || {
+              let specifics = match controls_state() {
+                  ControlsState::Error => "bg-red-100 text-red-800 border-red-800",
+                  ControlsState::Playing => "bg-blue-100 text-blue-800 border-blue-800",
+                  ControlsState::Stopped | ControlsState::Loading => {
+                      "border-gray-800 bg-gray-100 text-gray-800 hover:bg-gray-800 hover:text-white"
+                  }
+              };
+              format!("mr-4 rounded-full border my-1 py-2 px-4 flex items-center {}", specifics)
           }
-          ControlsState::Loading => {
+        >
 
-              view! { cx,
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  fill="currentColor"
-                  class="bi bi-hourglass-split inline"
-                  viewBox="0 0 16 16"
-                >
-                  <path d="M2.5 15a.5.5 0 1 1 0-1h1v-1a4.5 4.5 0 0 1 2.557-4.06c.29-.139.443-.377.443-.59v-.7c0-.213-.154-.451-.443-.59A4.5 4.5 0 0 1 3.5 3V2h-1a.5.5 0 0 1 0-1h11a.5.5 0 0 1 0 1h-1v1a4.5 4.5 0 0 1-2.557 4.06c-.29.139-.443.377-.443.59v.7c0 .213.154.451.443.59A4.5 4.5 0 0 1 12.5 13v1h1a.5.5 0 0 1 0 1h-11zm2-13v1c0 .537.12 1.045.337 1.5h6.326c.216-.455.337-.963.337-1.5V2h-7zm3 6.35c0 .701-.478 1.236-1.011 1.492A3.5 3.5 0 0 0 4.5 13s.866-1.299 3-1.48V8.35zm1 0v3.17c2.134.181 3 1.48 3 1.48a3.5 3.5 0 0 0-1.989-3.158C8.978 9.586 8.5 9.052 8.5 8.351z"></path>
-                </svg>
+          <span class="mr-2">{label}</span>
+          {move || match controls_state() {
+              ControlsState::Stopped => {
+                  view! { cx,
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      fill="currentColor"
+                      class="bi bi-play inline"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M10.804 8 5 4.633v6.734L10.804 8zm.792-.696a.802.802 0 0 1 0 1.392l-6.363 3.692C4.713 12.69 4 12.345 4 11.692V4.308c0-.653.713-.998 1.233-.696l6.363 3.692z"></path>
+                    </svg>
+                  }
               }
-          }
-      }}
+              ControlsState::Error | ControlsState::Playing => {
 
-    </button>
+                  view! { cx,
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      fill="currentColor"
+                      class="bi bi-stop-circle inline"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"></path>
+                      <path d="M5 6.5A1.5 1.5 0 0 1 6.5 5h3A1.5 1.5 0 0 1 11 6.5v3A1.5 1.5 0 0 1 9.5 11h-3A1.5 1.5 0 0 1 5 9.5v-3z"></path>
+                    </svg>
+                  }
+              }
+              ControlsState::Loading => {
+
+                  view! { cx,
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      fill="currentColor"
+                      class="bi bi-hourglass-split inline"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M2.5 15a.5.5 0 1 1 0-1h1v-1a4.5 4.5 0 0 1 2.557-4.06c.29-.139.443-.377.443-.59v-.7c0-.213-.154-.451-.443-.59A4.5 4.5 0 0 1 3.5 3V2h-1a.5.5 0 0 1 0-1h11a.5.5 0 0 1 0 1h-1v1a4.5 4.5 0 0 1-2.557 4.06c-.29.139-.443.377-.443.59v.7c0 .213.154.451.443.59A4.5 4.5 0 0 1 12.5 13v1h1a.5.5 0 0 1 0 1h-11zm2-13v1c0 .537.12 1.045.337 1.5h6.326c.216-.455.337-.963.337-1.5V2h-7zm3 6.35c0 .701-.478 1.236-1.011 1.492A3.5 3.5 0 0 0 4.5 13s.866-1.299 3-1.48V8.35zm1 0v3.17c2.134.181 3 1.48 3 1.48a3.5 3.5 0 0 0-1.989-3.158C8.978 9.586 8.5 9.052 8.5 8.351z"></path>
+                    </svg>
+                  }
+              }
+          }}
+
+        </button>
+      </Show>
+    </Suspense>
   }
 }
