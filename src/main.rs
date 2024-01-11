9db8c8db805603847baa677551cc3902a1b93037
Roel Bruggink <roel@jaroel.nl>
2024-01-11 15:44:48 +0100

    format like leptos itself

--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,59 @@
 #[cfg(feature = "ssr")]
 #[tokio::main]
 async fn main() {
-  use axum::routing::{get, post};
-  use axum::Router;
-  use dfm_site::app::*;
-  use dfm_site::fileserv::file_and_error_handler;
-  use leptos::logging::log;
-  use leptos::*;
-  use leptos_axum::{generate_route_list, LeptosRoutes};
-  use leptos_image::cache_app_images;
+        use axum::routing::{get, post};
+    use axum::Router;
+    use dfm_site::app::*;
+    use dfm_site::fileserv::file_and_error_handler;
+    use leptos::logging::log;
+    use leptos::*;
+    use leptos_axum::{generate_route_list, LeptosRoutes};
+    use leptos_image::cache_app_images;
 
-  use std::net::SocketAddr;
-  // use std::path::PathBuf;
-  // use tower_http::compression::CompressionLayer;
+    use std::net::SocketAddr;
+    // use std::path::PathBuf;
+    // use tower_http::compression::CompressionLayer;
 
-  // use tower_http::trace::TraceLayer;
+    // use tower_http::trace::TraceLayer;
 
-  // console_subscriber::init();
+    // console_subscriber::init();
 
-  // Setting get_configuration(None) means we'll be using cargo-leptos's env values
-  // For deployment these variables are:
-  // <https://github.com/leptos-rs/start-axum#executing-a-server-on-a-remote-machine-without-the-toolchain>
-  // Alternately a file can be specified such as Some("Cargo.toml")
-  // The file would need to be included with the executable when moved to deployment
-  let conf = get_configuration(None).await.unwrap();
-  let leptos_options = conf.leptos_options;
-  let root = leptos_options.site_root.clone();
-  cache_app_images(root, || view! { <App/> }, 2, || (), || ())
-    .await
-    .expect("Failed to cache images");
+    // Setting get_configuration(None) means we'll be using cargo-leptos's env values
+    // For deployment these variables are:
+    // <https://github.com/leptos-rs/start-axum#executing-a-server-on-a-remote-machine-without-the-toolchain>
+    // Alternately a file can be specified such as Some("Cargo.toml")
+    // The file would need to be included with the executable when moved to deployment
+    let conf = get_configuration(None).await.unwrap();
+    let leptos_options = conf.leptos_options;
+    let root = leptos_options.site_root.clone();
+    cache_app_images(root, || view! { <App/> }, 2, || (), || ())
+        .await
+        .expect("Failed to cache images");
 
-  let routes = generate_route_list(|| view! { <App/> });
+    let routes = generate_route_list(|| view! { <App/> });
 
-  // build our application with a route
-  let app = Router::new()
-    .route("/api/*fn_name", post(leptos_axum::handle_server_fns))
-    .leptos_routes(&leptos_options, routes, || view! { <App/> })
-    .route("/cache/image", get(leptos_image::image_cache_handler))
-    .fallback(file_and_error_handler)
-    // .layer(CompressionLayer::new())
-    // .layer(TraceLayer::new_for_http())
-    .with_state(leptos_options);
+    // build our application with a route
+    let app = Router::new()
+        .route("/api/*fn_name", post(leptos_axum::handle_server_fns))
+        .leptos_routes(&leptos_options, routes, || view! { <App/> })
+        .route("/cache/image", get(leptos_image::image_cache_handler))
+        .fallback(file_and_error_handler)
+        // .layer(CompressionLayer::new())
+        // .layer(TraceLayer::new_for_http())
+        .with_state(leptos_options);
 
-  let http_addr = std::env::var("HTTP_ADDR").unwrap_or("127.0.0.1:3002".to_string());
-  let http_addr = http_addr.parse::<SocketAddr>().expect("HTTP_ADDR");
-  log!("listening on http://{}", &http_addr);
-  axum::Server::bind(&http_addr)
-    .serve(app.into_make_service())
-    .await
-    .unwrap();
+    let http_addr = std::env::var("HTTP_ADDR").unwrap_or("127.0.0.1:3002".to_string());
+    let http_addr = http_addr.parse::<SocketAddr>().expect("HTTP_ADDR");
+    log!("listening on http://{}", &http_addr);
+    axum::Server::bind(&http_addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
 
 #[cfg(not(feature = "ssr"))]
 pub fn main() {
-  // no client-side main function
-  // unless we want this to work with e.g., Trunk for a purely client-side app
-  // see lib.rs for hydration function instead
+    // no client-side main function
+    // unless we want this to work with e.g., Trunk for a purely client-side app
+    // see lib.rs for hydration function instead
 }
--- src/routes/home.rs
+++ src/routes/home.rs
@@ -5,153 +5,235 @@ use leptos_router::*;
 
 #[component]
 fn Sponsor(href: String, title: String, children: Children) -> impl IntoView {
-  view! {
-    <div class="p-1 mb-2">
-      <a href=href title=title target="_blank">
-        {children()}
-      </a>
-    </div>
-  }
+    view! {
+        <div class="p-1 mb-2">
+            <a href=href title=title target="_blank">
+                {children()}
+            </a>
+        </div>
+    }
 }
 
 #[component]
 pub(crate) fn HomePage() -> impl IntoView {
-  view! {
-    <Player/>
-    <div class="flex justify-evenly mt-10 mb-10">
-      <div class="max-w-sm">
-        <div style="width: 384; height: 329">
-          <Image
-            src="/logodinxperfm.png"
-            alt="De markt van Dinxperlo"
-            width=384
-            height=329
-            quality=100
-            lazy=false
-            priority=true
-            class="w-[384] h-auto"
-          />
+    view! {
+        <Player/>
+        <div class="flex justify-evenly mt-10 mb-10">
+            <div class="max-w-sm">
+                <div style="width: 384; height: 329">
+                    <Image
+                        src="/logodinxperfm.png"
+                        alt="De markt van Dinxperlo"
+                        width=384
+                        height=329
+                        quality=100
+                        lazy=false
+                        priority=true
+                        class="w-[384] h-auto"
+                    />
+                </div>
+                <p class="text-center mt-4">"Het swingende geluid van Dinxperlo!"</p>
+            </div>
         </div>
-        <p class="text-center mt-4">"Het swingende geluid van Dinxperlo!"</p>
-      </div>
-    </div>
-    <nav class="flex justify-evenly bg-gray-100">
-      <ul class="flex flex-wrap list-none my-1">
-        <li>
-          <Controls
-            title="Luister naar Dinxper FM - Het swingende geluid van Dinxperlo!".into()
-            label="Luister live!".into()
-            src="https://stream.dinxperfm.nl/1".into()
-          />
-        </li>
-        <li>
-          <A class="inline-block mt-3 px-4 no-underline text-blue-700" href="/uzg">
-            "Uitzending gemist?"
-          </A>
-        </li>
-      </ul>
-    </nav>
-    <div class="mt-10 flex justify-center">
-      <div style="width: 1085; height: 656">
-        <Image
-          src="/programmering/WeekprogrammaDFM-20230304.jpg"
-          alt="Programmering vanaf 4 maart 2023 van Dinxperlo FM "
-          width=1085
-          height=656
-          quality=100
-          lazy=false
-          priority=true
-        />
-      </div>
-    </div>
-    <div class="text-center">
-      <a target="blank" class="text-blue-400" href="/programmering/WeekprogrammaDFM-20230304.jpg">
-        "In nieuwe pagina openen"
-      </a>
-    </div>
-    <div class="mt-10 text-center">
-      <h2 class="text-2xl">"Dinxper FM wordt mede mogelijk gemaakt door"</h2>
-      <div class="flex flex-row flex-wrap justify-evenly">
-        <Sponsor title="De markt van Dinxperlo".into() href="https://www.facebook.com/markt.dinxperlo/".into()>
-          <Image src="/sponsors/makt.jpg" alt="De markt van Dinxperlo" width=250 height=250 quality=100/>
-        </Sponsor>
-        <Sponsor title="Naaiatelier Monique Harmsen".into() href="http://www.naaiateliermoniqueharmsen.nl/".into()>
-          <Image src="/sponsors/logo13.jpg" alt="Naaiatelier Monique Harmsen" width=250 height=250 quality=100/>
+        <nav class="flex justify-evenly bg-gray-100">
+            <ul class="flex flex-wrap list-none my-1">
+                <li>
+                    <Controls
+                        title="Luister naar Dinxper FM - Het swingende geluid van Dinxperlo!".into()
+                        label="Luister live!".into()
+                        src="https://stream.dinxperfm.nl/1".into()
+                    />
+                </li>
+                <li>
+                    <A class="inline-block mt-3 px-4 no-underline text-blue-700" href="/uzg">
+                        "Uitzending gemist?"
+                    </A>
+                </li>
+            </ul>
+        </nav>
+        <div class="mt-10 flex justify-center">
+            <div style="width: 1085; height: 656">
+                <Image
+                    src="/programmering/WeekprogrammaDFM-20230304.jpg"
+                    alt="Programmering vanaf 4 maart 2023 van Dinxperlo FM "
+                    width=1085
+                    height=656
+                    quality=100
+                    lazy=false
+                    priority=true
+                />
+            </div>
+        </div>
+        <div class="text-center">
+            <a
+                target="blank"
+                class="text-blue-400"
+                href="/programmering/WeekprogrammaDFM-20230304.jpg"
+            >
+                "In nieuwe pagina openen"
+            </a>
+        </div>
+        <div class="mt-10 text-center">
+            <h2 class="text-2xl">"Dinxper FM wordt mede mogelijk gemaakt door"</h2>
+            <div class="flex flex-row flex-wrap justify-evenly">
+                <Sponsor
+                    title="De markt van Dinxperlo".into()
+                    href="https://www.facebook.com/markt.dinxperlo/".into()
+                >
+                    <Image
+                        src="/sponsors/makt.jpg"
+                        alt="De markt van Dinxperlo"
+                        width=250
+                        height=250
+                        quality=100
+                    />
+                </Sponsor>
+                <Sponsor
+                    title="Naaiatelier Monique Harmsen".into()
+                    href="http://www.naaiateliermoniqueharmsen.nl/".into()
+                >
+                    <Image
+                        src="/sponsors/logo13.jpg"
+                        alt="Naaiatelier Monique Harmsen"
+                        width=250
+                        height=250
+                        quality=100
+                    />
 
-        </Sponsor>
-        <Sponsor title="Podesta event supplies".into() href="https://podesta.nl/".into()>
-          <Image src="/sponsors/logo15.jpg" alt="Podesta event supplies" width=250 height=250 quality=100/>
+                </Sponsor>
+                <Sponsor title="Podesta event supplies".into() href="https://podesta.nl/".into()>
+                    <Image
+                        src="/sponsors/logo15.jpg"
+                        alt="Podesta event supplies"
+                        width=250
+                        height=250
+                        quality=100
+                    />
 
-        </Sponsor>
-        <Sponsor
-          title="Adviesbureau Roenhorst Dinxperlo".into()
-          href="https://www.facebook.com/Adviesbureau-Roenhorst-Dinxperlo-234893600009274/".into()
-        >
-          <Image src="/sponsors/logo16.jpg" alt="Adviesbureau Roenhorst Dinxperlo" width=250 height=250 quality=100/>
+                </Sponsor>
+                <Sponsor
+                    title="Adviesbureau Roenhorst Dinxperlo".into()
+                    href="https://www.facebook.com/Adviesbureau-Roenhorst-Dinxperlo-234893600009274/"
+                        .into()
+                >
+                    <Image
+                        src="/sponsors/logo16.jpg"
+                        alt="Adviesbureau Roenhorst Dinxperlo"
+                        width=250
+                        height=250
+                        quality=100
+                    />
 
-        </Sponsor>
-        <Sponsor
-          title="Landwinkel Smits Groenten & Fruit | Smits Groentekwekerij B.V. | Smits Groenten en Fruit".into()
-          href="http://www.groentekwekerij-smits.nl/".into()
-        >
-          <Image
-            src="/sponsors/logo18.jpg"
-            alt="Landwinkel Smits Groenten & Fruit | Smits Groentekwekerij B.V. | Smits Groenten en Fruit"
-            width=250
-            height=250
-            quality=100
-          />
-        </Sponsor>
-        <Sponsor
-          title="Te Grotenhuis - Afvalstraat - Zand & grind verkoop - Dinxperlo".into()
-          href="http://www.tegrotenhuisdinxperlo.nl/".into()
-        >
-          <Image
-            src="/sponsors/logo19.jpg"
-            alt="Te Grotenhuis - Afvalstraat - Zand & grind verkoop - Dinxperlo"
-            width=250
-            height=250
-            quality=100
-          />
-        </Sponsor>
-        <Sponsor title="Tiggelovend-Kok B.V.".into() href="https://www.tiggelovend-kok.nl/".into()>
-          <Image src="/sponsors/logo22.jpg" alt="Tiggelovend-Kok B.V." width=250 height=250 quality=100/>
-        </Sponsor>
-        <Sponsor
-          title="VVNF: Vereniging Viering Nationale Feest- en Gedenkdagen Dinxperlo".into()
-          href="http://www.vvnf.nl/".into()
-        >
-          <Image
-            src="/sponsors/logo23.jpg"
-            alt="VVNF: Vereniging Viering Nationale Feest- en Gedenkdagen Dinxperlo"
-            width=250
-            height=250
-            quality=100
-          />
-        </Sponsor>
-        <Sponsor
-          title="Jumbo Dinxperlo Leussink".into()
-          href="https://www.jumbo.com/content/jumbo-dinxperlo-heelweg/".into()
-        >
-          <Image src="/sponsors/jumbo.jpg" alt="Jumbo Dinxperlo Leussink" width=250 height=250 quality=100/>
-        </Sponsor>
-        <Sponsor title="Old Dutch Dinxperlo".into() href="https://www.olddutchdinxperlo.nl/".into()>
-          <Image src="/sponsors/olddutch.jpg" alt="Old Dutch Dinxperlo" width=250 height=250 quality=100/>
-        </Sponsor>
-        <Sponsor title="MA-Shops".into() href="https://www.ma-shops.nl/?ref=dinxperfm".into()>
-          <Image src="/sponsors/mashops.jpg" alt="MA-Shops" width=250 height=250 quality=100/>
-        </Sponsor>
-        <Sponsor
-          title="Expert Dinxperlo".into()
-          href="https://www.expert.nl/winkels/dinxperlo?gclid=EAIaIQobChMI1Jutxdrh4AIVzLztCh02DgFoEAAYASAAEgJwevD_BwE"
-              .into()
-        >
-          <Image src="/sponsors/expert.jpg" alt="Expert Dinxperlo" width=250 height=250 quality=100/>
-        </Sponsor>
-        <Sponsor title="Alswin".into() href="https://www.dinxperlo.nl".into()>
-          <Image src="/sponsors/alswingr.jpg" alt="Alswin" width=250 height=250 quality=100/>
-        </Sponsor>
-      </div>
-    </div>
-  }
+                </Sponsor>
+                <Sponsor
+                    title="Landwinkel Smits Groenten & Fruit | Smits Groentekwekerij B.V. | Smits Groenten en Fruit"
+                        .into()
+                    href="http://www.groentekwekerij-smits.nl/".into()
+                >
+                    <Image
+                        src="/sponsors/logo18.jpg"
+                        alt="Landwinkel Smits Groenten & Fruit | Smits Groentekwekerij B.V. | Smits Groenten en Fruit"
+                        width=250
+                        height=250
+                        quality=100
+                    />
+                </Sponsor>
+                <Sponsor
+                    title="Te Grotenhuis - Afvalstraat - Zand & grind verkoop - Dinxperlo".into()
+                    href="http://www.tegrotenhuisdinxperlo.nl/".into()
+                >
+                    <Image
+                        src="/sponsors/logo19.jpg"
+                        alt="Te Grotenhuis - Afvalstraat - Zand & grind verkoop - Dinxperlo"
+                        width=250
+                        height=250
+                        quality=100
+                    />
+                </Sponsor>
+                <Sponsor
+                    title="Tiggelovend-Kok B.V.".into()
+                    href="https://www.tiggelovend-kok.nl/".into()
+                >
+                    <Image
+                        src="/sponsors/logo22.jpg"
+                        alt="Tiggelovend-Kok B.V."
+                        width=250
+                        height=250
+                        quality=100
+                    />
+                </Sponsor>
+                <Sponsor
+                    title="VVNF: Vereniging Viering Nationale Feest- en Gedenkdagen Dinxperlo"
+                        .into()
+                    href="http://www.vvnf.nl/".into()
+                >
+                    <Image
+                        src="/sponsors/logo23.jpg"
+                        alt="VVNF: Vereniging Viering Nationale Feest- en Gedenkdagen Dinxperlo"
+                        width=250
+                        height=250
+                        quality=100
+                    />
+                </Sponsor>
+                <Sponsor
+                    title="Jumbo Dinxperlo Leussink".into()
+                    href="https://www.jumbo.com/content/jumbo-dinxperlo-heelweg/".into()
+                >
+                    <Image
+                        src="/sponsors/jumbo.jpg"
+                        alt="Jumbo Dinxperlo Leussink"
+                        width=250
+                        height=250
+                        quality=100
+                    />
+                </Sponsor>
+                <Sponsor
+                    title="Old Dutch Dinxperlo".into()
+                    href="https://www.olddutchdinxperlo.nl/".into()
+                >
+                    <Image
+                        src="/sponsors/olddutch.jpg"
+                        alt="Old Dutch Dinxperlo"
+                        width=250
+                        height=250
+                        quality=100
+                    />
+                </Sponsor>
+                <Sponsor
+                    title="MA-Shops".into()
+                    href="https://www.ma-shops.nl/?ref=dinxperfm".into()
+                >
+                    <Image
+                        src="/sponsors/mashops.jpg"
+                        alt="MA-Shops"
+                        width=250
+                        height=250
+                        quality=100
+                    />
+                </Sponsor>
+                <Sponsor
+                    title="Expert Dinxperlo".into()
+                    href="https://www.expert.nl/winkels/dinxperlo?gclid=EAIaIQobChMI1Jutxdrh4AIVzLztCh02DgFoEAAYASAAEgJwevD_BwE"
+                        .into()
+                >
+                    <Image
+                        src="/sponsors/expert.jpg"
+                        alt="Expert Dinxperlo"
+                        width=250
+                        height=250
+                        quality=100
+                    />
+                </Sponsor>
+                <Sponsor title="Alswin".into() href="https://www.dinxperlo.nl".into()>
+                    <Image
+                        src="/sponsors/alswingr.jpg"
+                        alt="Alswin"
+                        width=250
+                        height=250
+                        quality=100
+                    />
+                </Sponsor>
+            </div>
+        </div>
+    }
 }
