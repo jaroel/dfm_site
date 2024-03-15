c6af4456eec1e98378a9bfce9553bb47239632f8
Roel Bruggink <roel@jaroel.nl>
2024-03-15 23:19:48 +0100

    More caching headers

--- app.config.ts
+++ app.config.ts
@@ -16,6 +16,18 @@ export default defineConfig({
         "/_build/assets/**": {
           headers: { "cache-control": "max-age=31536000, immutable" },
         },
+        "/_server/assets/**": {
+          headers: { "cache-control": "max-age=31536000, immutable" },
+        },
+        "/assets/**": {
+          headers: { "cache-control": "max-age=31536000, immutable" },
+        },
+        "/": {
+          headers: { "cache-control": "private,max-age=3600" },
+        },
+        "/uzg/": {
+          headers: { "cache-control": "max-age=3600" },
+        },
       },
     },
 
