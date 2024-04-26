1ffb908e0b815acc0d1253565814236f3bfb4d64
Roel Bruggink <roel@jaroel.nl>
2024-04-26 22:33:06 +0200

    Add solid-devtools

--- app.config.ts
+++ app.config.ts
@@ -1,31 +1,37 @@
 import { defineConfig } from "@solidjs/start/config";
 import { imagetools } from "vite-imagetools";
+import devtools from "solid-devtools/vite";
 
 export default defineConfig({
   vite: {
-    plugins: [imagetools()],
+    plugins: [
+      imagetools(),
+      devtools({
+        /* features options - all disabled by default */
+        autoname: true, // e.g. enable autoname
+      }),
+    ],
     build: {
       reportCompressedSize: false,
     },
   },
-    server: {
-      routeRules: {
-        "/_build/assets/**": {
-          headers: { "cache-control": "max-age=31536000, immutable" },
-        },
-        "/_server/assets/**": {
-          headers: { "cache-control": "max-age=31536000, immutable" },
-        },
-        "/assets/**": {
-          headers: { "cache-control": "max-age=31536000, immutable" },
-        },
-        "/": {
-          headers: { "cache-control": "max-age=3600, must-revalidate" },
-        },
-        "/uzg/": {
-          headers: { "cache-control": "max-age=3600" },
-        },
+  server: {
+    routeRules: {
+      "/_build/assets/**": {
+        headers: { "cache-control": "max-age=31536000, immutable" },
+      },
+      "/_server/assets/**": {
+        headers: { "cache-control": "max-age=31536000, immutable" },
+      },
+      "/assets/**": {
+        headers: { "cache-control": "max-age=31536000, immutable" },
+      },
+      "/": {
+        headers: { "cache-control": "max-age=3600, must-revalidate" },
+      },
+      "/uzg/": {
+        headers: { "cache-control": "max-age=3600" },
       },
     },
-
+  },
 });
--- src/entry-client.tsx
+++ src/entry-client.tsx
@@ -1,3 +1,4 @@
 import { StartClient, mount } from "@solidjs/start/client";
+import "solid-devtools";
 
 mount(() => <StartClient />, document.getElementById("app"));
