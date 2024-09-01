66a3a0f5c5716f85d13e2a2c462f3722e6df092a
Roel Bruggink <roel@jaroel.nl>
2024-09-01 14:04:37 +0200

    Use regular stream url

--- src/routes/(home).tsx
+++ src/routes/(home).tsx
@@ -31,7 +31,7 @@ export default function Home() {
             <Controls
               title="Luister naar Dinxper FM - Het swingende geluid van Dinxperlo!"
               label="Luister live!"
-              src="https://stream-dinxperfm-nl.toffe.site/1"
+              src="https://stream.dinxperfm.nl/1"
             />
           </li>
           <li>
