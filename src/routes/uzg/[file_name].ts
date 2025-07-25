c3068713fe2e949c1c502167183b6eeca7fe7869
Roel Bruggink <roel@jaroel.nl>
2025-07-25 16:28:52 +0200

    Revert "cache-control max-age=31536000, immutable for ftp mp3 files" Will fill disk
    This reverts commit 95ae98106eedb9751612c8760f32c4d5aec4d599.

--- src/routes/uzg/[file_name].ts
+++ src/routes/uzg/[file_name].ts
@@ -22,7 +22,6 @@ export async function GET({ params, request }: APIEvent) {
         "content-type": "audio/mpeg",
         "accept-ranges": "bytes",
         "content-length": ftp_data.metadata.size,
-        "cache-control": "max-age=31536000, immutable",
       },
     });
   }
