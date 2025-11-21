9e7009023cc9f95640086996acdace05215eb94c
Roel Bruggink <roel@jaroel.nl>
2025-11-21 17:55:36 +0100

    Make Picture use existing types

--- src/components/Picture.tsx
+++ src/components/Picture.tsx
@@ -1,24 +1,26 @@
 import type { Picture as TPicture } from "imagetools-core";
+import type { JSX } from "solid-js";
+import { For } from "solid-js";
 export type { TPicture };
+
+type Image = JSX.ImgHTMLAttributes<HTMLImageElement>;
 
 export default function Picture(props: {
   logo: TPicture;
-  alt: string;
-  class?: string;
-  style?: string;
-  fetchpriority?: "high" | "low" | "auto";
-}) {
+} & Pick<Image, "alt"> & Partial<Pick<Image, "class"|"style"|"fetchpriority"|"decoding"|"loading">>) {
   return (
-    <picture class={`inline-block ${props.class}`} style={props.style}>
-      {Object.entries(props.logo.sources).map(([format, images]) => (
-        <source srcset={images} type={`image/${format}`} />
-      ))}
+    <picture class={`inline-block ${props.class ?? ""}`} style={props.style}>
+      <For each={Object.entries(props.logo.sources)}>
+        {([format, images]) => <source srcset={images} type={`image/${format}`} />}
+      </For>
       <img
         src={props.logo.img.src}
         alt={props.alt}
         width={props.logo.img.w}
         height={props.logo.img.h}
         fetchpriority={props.fetchpriority}
+        decoding={props.decoding}
+        loading={props.loading}
       />
     </picture>
   );
