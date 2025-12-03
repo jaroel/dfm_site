d2416f83f220abe342a74c970af194cc0d871536
Roel Bruggink <roel@jaroel.nl>
2025-12-03 21:26:30 +0100

    remove matomo for now

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -3,20 +3,6 @@ import { createHandler, StartServer } from "@solidjs/start/server";
 import favicon from "~/assets/favicon.ico";
 
 
-const MATOMO_SCRIPT_CONTENT = `
-  var _paq = window._paq = window._paq || [];
-  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-  _paq.push(['trackPageView']);
-  _paq.push(['enableLinkTracking']);
-  (function() {
-    var u="//web.matomo.orb.local/";
-    _paq.push(['setTrackerUrl', u+'matomo.php']);
-    _paq.push(['setSiteId', '1']);
-    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
-  })();
-`;
-
 export default createHandler(() => (
   <StartServer
     document={({ assets, children, scripts }) => (
@@ -26,7 +12,6 @@ export default createHandler(() => (
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="icon" href={favicon} />
           {assets}
-          <script>{MATOMO_SCRIPT_CONTENT}</script>
         </head>
         <body>
           <div id="app">{children}</div>
