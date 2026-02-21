374b416fe49331d0a6fa79f3ac01e6ba960de70e
Roel Bruggink <roel@jaroel.nl>
2026-02-21 23:40:15 +0100

    remove some sponsors

--- src/views/home.rs
+++ src/views/home.rs
@@ -1,6 +1,9 @@
 use dioxus::prelude::*;
 
-use crate::{Route, components::{controls::Controls, player::Player}};
+use crate::{
+    components::{controls::Controls, player::Player},
+    Route,
+};
 
 const LOGO: Asset = asset!(
     "/assets/logodinxperfm.png",
@@ -83,66 +86,6 @@ const IMAGE_250_120: ImageSize = ImageSize::Manual {
     width: 250,
     height: 120,
 };
-const LOGO_MARKT: Asset = asset!(
-    "/assets/sponsors/makt.jpg",
-    ImageAssetOptions::new().with_size(IMAGE_250_100)
-);
-const LOGO_13: Asset = asset!(
-    "/assets/sponsors/logo13.jpg",
-    ImageAssetOptions::new().with_size(IMAGE_250_120)
-);
-const LOGO_15: Asset = asset!(
-    "/assets/sponsors/logo15.jpg",
-    ImageAssetOptions::new().with_size(IMAGE_250_120)
-);
-const LOGO_16: Asset = asset!(
-    "/assets/sponsors/logo16.jpg",
-    ImageAssetOptions::new().with_size(IMAGE_250_120)
-);
-const LOGO_DEDRIESPRONG: Asset = asset!(
-    "/assets/sponsors/dedriesprong.jpg",
-    ImageAssetOptions::new().with_size(ImageSize::Manual {
-        width: 250,
-        height: 65
-    })
-);
-const LOGO_TIBATEK: Asset = asset!(
-    "/assets/sponsors/tibatek_logo_web.png",
-    ImageAssetOptions::new().with_size(ImageSize::Manual {
-        width: 250,
-        height: 55
-    })
-);
-const LOGO_BLAUWE_MEER: Asset = asset!(
-    "/assets/sponsors/logo-blauwe-meer.png",
-    ImageAssetOptions::new().with_size(ImageSize::Manual {
-        width: 250,
-        height: 160
-    })
-);
-const LOGO_RICO: Asset = asset!(
-    "/assets/sponsors/etenbijrico.jpg",
-    ImageAssetOptions::new().with_size(IMAGE_250_120)
-);
-const LOGO_HARMTAKKE: Asset = asset!(
-    "/assets/sponsors/logo-harmtakke.jpg",
-    ImageAssetOptions::new().with_size(ImageSize::Manual {
-        width: 250,
-        height: 88
-    })
-);
-const LOGO_MASHOPS: Asset = asset!(
-    "/assets/sponsors/mashops.jpg",
-    ImageAssetOptions::new().with_size(IMAGE_250_120)
-);
-const LOGO_OLDDUTCH: Asset = asset!(
-    "/assets/sponsors/olddutch.jpg",
-    ImageAssetOptions::new().with_size(IMAGE_250_120)
-);
-const LOGO_LOGO23: Asset = asset!(
-    "/assets/sponsors/logo23.jpg",
-    ImageAssetOptions::new().with_size(IMAGE_250_120)
-);
 
 #[component]
 fn Sponsors() -> Element {
@@ -158,18 +101,10 @@ fn Sponsors() -> Element {
                         href: "https://www.facebook.com/markt.dinxperlo/",
                         img {
                             alt: "Logo van de markt van Dinxperlo",
-                            src: LOGO_MARKT,
-                            width: 250,
-                        }
-                    }
-                }
-                div { class: "p-1 mb-2",
-                    a {
-                        target: "blank",
-                        href: "http://www.naaiateliermoniqueharmsen.nl/",
-                        img {
-                            src: LOGO_13,
-                            alt: "Logo van Naai Atelier Monique Harmsen",
+                            src: asset!(
+                                "/assets/sponsors/makt.jpg",
+                                ImageAssetOptions::new().with_size(IMAGE_250_100)
+                            ),
                             width: 250,
                         }
                     }
@@ -178,7 +113,10 @@ fn Sponsors() -> Element {
                     a { href: "https://podesta.nl/", target: "blank",
                         img {
                             alt: "Logo van Podesta",
-                            src: LOGO_15,
+                            src: asset!(
+                                "/assets/sponsors/logo15.jpg",
+                                ImageAssetOptions::new().with_size(IMAGE_250_120)
+                            ),
                             width: 250,
                         }
                     }
@@ -189,7 +127,10 @@ fn Sponsors() -> Element {
                         href: "https://www.facebook.com/Adviesbureau-Roenhorst-Dinxperlo-234893600009274/",
                         img {
                             alt: "Logo van Adviesbureau Roenhorst",
-                            src: LOGO_16,
+                            src: asset!(
+                                "/assets/sponsors/logo16.jpg",
+                                ImageAssetOptions::new().with_size(IMAGE_250_120)
+                            ),
                             width: 250,
                         }
                     }
@@ -198,29 +139,10 @@ fn Sponsors() -> Element {
                     a { href: "http://www.vvnf.nl/home", target: "blank",
                         img {
                             alt: "Logo van VVNF",
-                            src: LOGO_LOGO23,
-                            width: 250,
-                        }
-                    }
-                }
-                div { class: "p-1 mb-2",
-                    a {
-                        href: "https://www.olddutchdinxperlo.nl/",
-                        target: "blank",
-                        img {
-                            src: LOGO_OLDDUTCH,
-                            alt: "Logo van Old Dutch",
-                            width: 250,
-                        }
-                    }
-                }
-                div { class: "p-1 mb-2",
-                    a {
-                        href: "https://www.ma-shops.nl/?ref=dinxperfm",
-                        target: "blank",
-                        img {
-                            src: LOGO_MASHOPS,
-                            alt: "Logo van MA-Shops",
+                            src: asset!(
+                                "/assets/sponsors/logo23.jpg",
+                                ImageAssetOptions::new().with_size(IMAGE_250_120)
+                            ),
                             width: 250,
                         }
                     }
@@ -228,26 +150,14 @@ fn Sponsors() -> Element {
                 div { class: "p-1 mb-2",
                     a { href: "https://harmtakke.nl", target: "blank",
                         img {
-                            src: LOGO_HARMTAKKE,
+                            src: asset!(
+                                "/assets/sponsors/logo-harmtakke.jpg",
+                                ImageAssetOptions::new().with_size(ImageSize::Manual {
+                                    width: 250,
+                                    height: 88
+                                })
+                            ),
                             alt: "Logo van Harm Takke",
-                            width: 250,
-                        }
-                    }
-                }
-                div { class: "p-1 mb-2",
-                    a { href: "https://etenbijrico.nl", target: "blank",
-                        img {
-                            src: LOGO_RICO,
-                            alt: "Logo van Eten bij Rico",
-                            width: 250,
-                        }
-                    }
-                }
-                div { class: "p-1 mb-2",
-                    a { target: "blank", href: "https://hetblauwemeer.nl",
-                        img {
-                            src: LOGO_BLAUWE_MEER,
-                            alt: "Logo van Het Blauwe Meer",
                             width: 250,
                         }
                     }
@@ -255,7 +165,13 @@ fn Sponsors() -> Element {
                 div { class: "p-1 mb-2",
                     a { href: "https://tibatek.de", target: "blank",
                         img {
-                            src: LOGO_TIBATEK,
+                            src: asset!(
+                                "/assets/sponsors/tibatek_logo_web.png",
+                                ImageAssetOptions::new().with_size(ImageSize::Manual {
+                                    width: 250,
+                                    height: 55
+                                })
+                            ),
                             width: 250,
                             alt: "Logo van Tibatek",
                             class: "bg-white p-1",
@@ -268,7 +184,13 @@ fn Sponsors() -> Element {
                         href: "https://www.facebook.com/CafetariadeDriesprongDinxperlo/?locale=nl_NL",
                         img {
                             alt: "Logo van Cafetaria de Driesprong",
-                            src: LOGO_DEDRIESPRONG,
+                            src: asset!(
+                                "/assets/sponsors/dedriesprong.jpg",
+                                ImageAssetOptions::new().with_size(ImageSize::Manual {
+                                    width: 250,
+                                    height: 65
+                                })
+                            ),
                             width: 250,
                         }
                     }
