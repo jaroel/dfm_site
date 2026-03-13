948df92ceabaa91774a54180d6b25302ae1bffe4
Roel Bruggink <roel@jaroel.nl>
2026-03-13 21:51:45 +0100

    cleanup

--- src/routes/uzg/[file_name].ts
+++ src/routes/uzg/[file_name].ts
@@ -1,5 +1,5 @@
 import type { APIEvent } from "@solidjs/start/server";
-import { FilenameDotMp3, getFtpStream } from "~/ftp.ts";
+import { getFtpStream } from "~/ftp.ts";
 
 export async function GET({ params, request }: APIEvent) {
   const filename = params.file_name.trim();
